50b8036d0e329a566be05f986102a2eb7bc0f11a
Shawn <unshorn@gmail.com>
2021-10-04 08:34:44 +1300

    add files

---
+++ src/main/java/testingTutorial/Student.java
@@ -0,0 +1,79 @@
+package testingTutorial;
+
+public class Student {
+
+	private int id = 0;
+	private String firstName = null;
+	private String name = "";
+	
+	public Student(int id, String firstName, String name) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.name = name;
+	}
+	
+	public Student() {
+		super();
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", firstName=" + firstName + ", name="
+				+ name + "]";
+	}
+	
+
+}
