4e079f3da9f0296c7c160ef01a2ceb6e21b9f91a
fletchthefletch <fletcher@4tradeit.co.nz>
2021-10-09 14:15:25 +1300

    Cleaned

--- src/test/java/testingTutorial/CaseTester.java
+++ src/test/java/testingTutorial/CaseTester.java
@@ -3,8 +3,6 @@ package testingTutorial;
 import org.junit.jupiter.api.Test;
 import static
         org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
@@ -14,8 +12,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.IdentityHashMap;
 import java.util.List;
-
-import org.junit.jupiter.api.BeforeAll;
 
 public class CaseTester {
 	//** Lecturer vs Student Contractual Violation **//
